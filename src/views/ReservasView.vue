61d051b23e99fcfa1ab5bffec2be3b2a93cf93ae
Anderson Lima De Menezes <86325561+And3rsLMM@users.noreply.github.com>
2022-12-08 21:30:07 -0300

    Revert "Fcervant"

--- src/views/ReservasView.vue
+++ src/views/ReservasView.vue
@@ -1,51 +1,16 @@
-<!-- utilizar como <base href="// /home/fernandorc/Documentos/aulaVUE/projeto_spa" -->
-<!-- utilizar como <base href="// /home/fernandorc/Documentos/aulaVUE/projeto_spa" -->
-
 <template>
-  <div class="reservas">
-    <h1>Teste - branch FCERVANT</h1>
-  </div>
-  <div>
-    <FormReserva />
-  </div>
-  <hr />
-  <div>
-    <PainelAcomodacoes />
-  </div>
-  <hr />
-  <div>
-    <PainelReserva />
-  </div>
-  <hr />
-  <div>
-    <ModalConfirma />
-  </div>
-</template>
+    <div class="reservas">
+        <p>Teste - branch FCERVANT</p>
+    </div>    
+</template>   
 
 <script>
-import FormReserva from "/src/components/reserva/FormReserva.vue";
-import PainelAcomodacoes from "/src/components/reserva/PainelAcomodacoes.vue";
-import PainelReserva from "/src/components/reserva/PainelReserva.vue";
-import ModalConfirma from "/src/components/reserva/ModalConfirma.vue";
 
-export default {
-  name: "ReservasView",
-  data() {
-    // data
-    return {
-      // return
-    };
-  },
-  components: { FormReserva, PainelReserva, PainelAcomodacoes, ModalConfirma },
-};
 </script>
 
 <style scoped>
+
 .reservas p {
-  color: red;
-}
-.reservas h1 {
-  color: brown;
-  margin: 5px 5px 5px 5px;
+    color: red;
 }
 </style>
